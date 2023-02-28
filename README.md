f722acd295c07f8af4a38b4766e855ee88a772dc
Your Name <you@example.com>
2023-02-28 19:59:25 +0000

    added source

---
+++ README.md
@@ -0,0 +1,46 @@
+# RustWERBreakChain
+"simple PoC for self-breaking own parent-child process chain" by gtworek.
+
+Re-wrote it using the windows-sys Rust crate.
+
+
+![img.png](img.png)
+
+## Getting Started
+
+### Dependencies
+```
+[dependencies.windows-sys]
+version = "0.45.0"
+features = [
+    "Win32_Foundation",
+    "Win32_System_Recovery",
+]
+```
+
+### Executing program
+
+building
+```
+cargo build --release
+```
+Running
+```
+cargo run --release
+```
+
+## Help
+
+Make sure WER (Windows error reporting is active on the machine)
+```
+PS C:\Windows\system32> Enable-WindowsErrorReporting
+True
+```
+
+## author
+- PUNICODE_STRING
+
+## Acknowledgments
+Original discovery
+* https://github.com/gtworek @0gtweet
+* https://twitter.com/0gtweet/status/1629967880392458242
---
+++ src/main.rs
@@ -0,0 +1,53 @@
+// https://github.com/gtworek/PSBits/blob/master/Misc/BreakChain.c
+// https://cs.github.com/microsoft/windows-rs/blob/371245db0fd703842b3f6351a8ea71aa46d1bd0e/crates/libs/windows/src/Windows/Win32/System/Recovery/mod.rs?q=language%3Arust+RegisterApplicationRestart#L48
+// https://cs.github.com/microsoft/windows-rs?q=
+// https://stackoverflow.com/questions/74518825/strange-process-names-when-trying-to-get-a-pid-in-rust-with-the-windows-api
+
+/*
+Full credits go to gtworek
++rep for the cool parent process de-chaining
+*/
+use std::{
+    env::args,
+    process::{exit, abort},
+    thread::sleep,
+    time::Duration,
+    process::{Command, ExitStatus},
+    io::Result
+};
+
+use windows_sys::{
+    Win32::System::Recovery::RegisterApplicationRestart,
+    core::*,
+    Win32::Foundation::S_OK,
+};
+
+static SLEEP_MS:Duration  = Duration::from_millis(62 * 1000);
+static POST_SLEEP_MS:Duration  = Duration::from_millis(100000);
+
+pub fn execute(exe: &str, args: &[&str]) -> Result<ExitStatus> {
+    Command::new(exe).args(args).spawn()?.wait()
+}
+
+fn main() {
+    let args:Vec<String> = args().collect();
+    if args.len() == 1 {
+        unsafe {
+            let pcwstr_name:PCWSTR = w!("doit");
+            let h_result:HRESULT = RegisterApplicationRestart(
+                pcwstr_name,
+                0
+            );
+            if h_result != S_OK{
+                println!("error {:?}", h_result);
+                exit(h_result);
+            }
+            println!("Original parent, sleeping to make WER happy...");
+            sleep(SLEEP_MS);
+            abort();
+        }
+    } else {
+        println!("Monkey");
+        sleep(POST_SLEEP_MS);
+    }
+}
